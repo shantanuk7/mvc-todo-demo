b257ace6a5d2c8665d626c7a869d25dc3fc8e2f5
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-03 23:29:11 +0530

    fix(routes): Correct the express router initialization

--- routes/task.route.js
+++ routes/task.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express();
+const router = express.Router();
 const taskController = require('../controllers/task.controller.js');
 
 router.post('/', taskController.createTask);
