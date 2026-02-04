8c2df29f158051ed4c68cdf86a6d61c962f0361d
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-04 13:48:15 +0530

    fix(app.js): add urlencoded middleware for form submissions
    Adds a express middleware that allows parsing application/x-www-form-urlencoded requests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const taskRoutes = require('../routes/task.route.js')
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/v1/tasks', taskRoutes);
 module.exports = app;
