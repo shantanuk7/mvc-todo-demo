6d89a9e83a6eefcd34ae4e22e942403e794d8021
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-04 15:57:22 +0530

    feat: add route for patch update task partially

--- routes/task.route.js
+++ routes/task.route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/task.controller.js');
 const validateTaskMiddleware = require('../middlewares/task.middleware.js');
+const { route } = require('../src/app.js');
 router.post('/', validateTaskMiddleware, taskController.createTask);
 router.get('/', taskController.getAllTasks);
+router.patch('/:id', taskController.updateTask);
 
 module.exports = router;
