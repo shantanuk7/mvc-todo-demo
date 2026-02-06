eadc672e95e0613ae0ed0ca2b416e03793d7c462
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-06 15:37:45 +0530

    feat: add delete task controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -32,9 +32,6 @@ const getAllTasks = async (req, res) => {
     }
 }
 
-
-
-
 const updateTask = async (req, res) => {
     const taskId = req.params.id;
     const { title, description, status, priority } = req.body;
@@ -61,8 +58,6 @@ const updateTask = async (req, res) => {
     }
 };
 
-
-
 const getTaskById = async (req, res) => {
     const taskId = req.params.id;
 
@@ -79,9 +74,26 @@ const getTaskById = async (req, res) => {
     }
 };
 
+const deleteTaskById = async (req,res) =>{
+    const taskId =req.params.id;
+    try{
+        await service.deleteTaskById(taskId);
+        res.status(200).send(result);
+    }
+    catch(error){
+        res.status(404).json({
+            "error": {
+                "code": "TASK_NOT_FOUND",
+                "message": error.message
+            }
+        });
+    }
+}
+
 module.exports = {
     createTask,
     getAllTasks,
     updateTask,
-    getTaskById
+    getTaskById,
+    deleteTaskById
 };
