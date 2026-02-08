19e6572eca9d44bfe99e73bbf66617b76b3a721c
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-08 22:30:28 +0530

    feat:add bulk tasks route

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -3,9 +3,10 @@
 const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/task.controller.js');
-const { validateTask, validateUpdateTask } = require('../middlewares/task.middleware.js');
+const { validateTask, validateUpdateTask, validateBulkTasks } = require('../middlewares/task.middleware.js');
 
 router.post('/', validateTask, taskController.createTask);
+router.post('/bulk', validateBulkTasks, taskController.createTasks);
 router.get('/', taskController.getAllTasks);
 router.patch('/:id', validateUpdateTask, taskController.updateTask);
 router.get('/:id', taskController.getTaskById);
