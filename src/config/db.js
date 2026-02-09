bdcb8674fa64cd036962701352bb976cb3ee45d1
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-09 14:57:07 +0530

    feat: create table from the db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql2/promise");
 
+let pool;
+
 const initDB = async () => {
     const connection = await mysql.createConnection({
         host: process.env.DB_HOST,
@@ -8,11 +10,36 @@ const initDB = async () => {
     });
 
     const dbName = process.env.DB_NAME;
-
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${dbName}\`;`);
-    console.log(`Successfully created/verified database: ${dbName}`);
-
     await connection.end();
+    console.log(`Database '${dbName}' is ready.`);
+    pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: dbName,
+    });
+
+    const tableQuery = `
+        CREATE TABLE IF NOT EXISTS tasks (
+            id VARCHAR(255) PRIMARY KEY,
+            title VARCHAR(100) NOT NULL,
+            description VARCHAR(500),
+            status ENUM('pending', 'in progress', 'completed') DEFAULT 'pending',
+            priority ENUM('low', 'medium', 'high') DEFAULT 'low',
+            createdAt VARCHAR(255),
+            updatedAt VARCHAR(255)
+        );
+    `;
+    await pool.execute(tableQuery);
+    console.log('Tasks table is ready.');
 };
 
-module.exports = { initDB };
+const getDB = () => {
+    if (!pool) {
+        throw new Error("Database not initialized. Call initDB first.");
+    }
+    return pool;
+};
+
+module.exports = { initDB, getDB };
